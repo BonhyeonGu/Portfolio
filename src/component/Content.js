112396aba9d991d4b89bcd886c90d178f4030e0a
whwhdtk <8whwhdtk8@naver.com>
2022-07-02 15:50:10 +0900

    바디 정리

--- src/component/Content.js
+++ src/component/Content.js
@@ -3,11 +3,8 @@ import CAbout from './CAbout.js';
 import CProject from './CProject.js';
 export default function Content() {
     return <div className={styles.box}>
-        <div id="Home">
-            <CAbout />
-        </div>
         <div id="About">
-            <h2>2</h2>
+            <CAbout />
         </div>
         <div id="Teck">
             <h2>3</h2>
