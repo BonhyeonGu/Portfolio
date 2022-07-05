da1b955ac0b5b7d606245b6de6406b01e5192fbe
BonhyeonGu <tayasriel@gmail.com>
2022-07-05 17:03:22 +0900

    배지 추가

--- src/component/CAbout.js
+++ src/component/CAbout.js
@@ -2,6 +2,7 @@ import styles from "./CAbout.module.css";
 import React, {useState, useEffect, useRef} from 'react';
 import FadeInOut from "./FadeInOut";
 import Typewriter from 'typewriter-effect';
+import ReactMarkdown from 'react-markdown';
 
 export default function CAbout() {
     const [show, setShow] = useState(true);
@@ -39,24 +40,29 @@ export default function CAbout() {
       toggleShow();
     }, 2000)
 
-    const github = 'https://img.shields.io/badge/-debian-A81D33';
-
     return (
-      <div className={styles.box}>
-        <div className={styles.fake} />
-        <img src="img/a1.png" className={styles.img} />
-        <div className={styles.title}><Typewriter
-        onInit={(typewriter) => {
-          typewriter.typeString('반갑습니다. 배고픈')
-            .pauseFor(300)
-            .deleteChars(3)	
-            .typeString('목마른 서버 개발자 <b>구본현</b>입니다.')
-            .pauseFor(2500)
-            .start();
-        }}/></div>
-        <br />
-        <img src={github}></img>
-        <div className={styles.texts}></div>
-      </div> 
+        <div className={styles.box}>
+            <div className={styles.fake} />
+            <img src="img/a1.png" className={styles.img} />
+            <div className={styles.title}><Typewriter
+            onInit={(typewriter) => {
+            typewriter.typeString('반갑습니다. 배고픈')
+                .pauseFor(300)
+                .deleteChars(3)	
+                .typeString('목마른 서버 개발자 <b>구본현</b>입니다.')
+                .pauseFor(2500)
+                .start();
+            }}/></div>
+            <hr />
+            <a href="https://github.com/BonhyeonGu" target="_blank"><img src="https://img.shields.io/badge/github-000000?style=for-the-badge&logo=github&logoColor=white" className={styles.shield} /></a>&nbsp;&nbsp; 
+            <a href="https://docs.9bon.org/" target="_blank"><img src="https://img.shields.io/badge/dev_blog-000000?style=for-the-badge&logo=hugo&logoColor=white" className={styles.shield} /></a>&nbsp;&nbsp;
+            <a href="https://github.com/BonhyeonGu" target="_blank"><img src="https://img.shields.io/badge/youtube-000000?style=for-the-badge&logo=youtube&logoColor=white" className={styles.shield} /></a>
+            <hr />
+            <div className={styles.texts}>
+                일편단심 네트워크-서버 프로그래밍 전공을 하고 있습니다. Quagga, Iptable를 사용한 망 구축, 분리<br />
+                Samba, Bind9, AD를 이용한 DC, 그리고 이에 맞는 각종 서비스를 만들어가고 있습니다.<br />
+                
+            </div>
+        </div>
     )
 }
