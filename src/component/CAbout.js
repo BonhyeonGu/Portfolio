430c021a52acf855eccaaf245d61d9ad8782b9ec
whwhdtk <8whwhdtk8@naver.com>
2023-11-15 01:07:14 +0900

    슬라이드쇼 외부 패키지 삭제 및 본인 코드 적용

--- src/component/CAbout.js
+++ src/component/CAbout.js
@@ -2,46 +2,52 @@ import styles from "./CAbout.module.css";
 import React, {useState, useEffect, useRef} from 'react';
 import Typewriter from 'typewriter-effect';
 
-import { Fade } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
-
-
-
 export default function CAbout() {
     const slideImages = [
-        {
-            url: 'img/a1.png',
-            caption: 'Slide 1',
-        },
-        {
-            url: 'img/a2.png',
-            caption: 'Slide 2'
-        },
-        {
-            url: 'img/a3.png',
-            caption: 'Slide 3'
-        },
+        { url: 'img/a1.png', caption: '슬라이드 1' },
+        { url: 'img/a2.png', caption: '슬라이드 2' },
+        { url: 'img/a3.png', caption: '슬라이드 3' },
     ];
+
+    // 현재 슬라이드 인덱스를 위한 상태
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [isFading, setIsFading] = useState(false);
+    useEffect(() => {
+        setIsFading(true); // 페이드 인 시작
+        const timer = setInterval(() => {
+            setIsFading(false); // 페이드 아웃 시작
+            setTimeout(() => {
+                setCurrentSlide((prevSlide) => prevSlide === slideImages.length - 1 ? 0 : prevSlide + 1);
+            }, 3000); // 페이드 아웃 시간과 맞춰야 함
+        }, 5000);
+    
+        // 컴포넌트가 언마운트될 때 인터벌을 정리합니다
+        return () => clearInterval(timer);
+    }, [currentSlide]); // 의존성 배열에 currentSlide 추가
+    
     const endHeight = {
         height: "140px",
         display: "inline-block"
     }
+
     return (
         <div className={styles.box}>
             {/*<div className={styles.fake} />*/}
-            <div className="slide-container">
-                <Fade>
-                    {slideImages.map((slideImage, index)=> (
-                        <div className="each-fade" key={index}>
-                            <div style={{'backgroundImage': `url(${slideImage.url})`}}>
-                                <div className={styles.fakeImg} />
-                            </div>
-                        </div>
-                    ))} 
-                </Fade>
+            <div className={styles.slideshow}>
+                {slideImages.map((slide, index) => (
+                    <img
+                        key={index}
+                        src={slide.url}
+                        alt={slide.caption}
+                        className={styles.slideImage}
+                        style={{
+                            opacity: currentSlide === index ? 1 : 0,
+                            zIndex: currentSlide === index ? 1 : -1
+                        }}
+                    />
+                ))}
             </div>
             <br />
-            {/*<img src="img/a1.png" className={styles.img} />*/}
             <div className={styles.title}><Typewriter
             onInit={(typewriter) => {
             typewriter.typeString('반갑습니다. 배고픈')
@@ -54,10 +60,9 @@ export default function CAbout() {
             <hr />
             <a href="https://github.com/BonhyeonGu" target="_blank"><img src="https://img.shields.io/badge/github-000000?style=for-the-badge&logo=github&logoColor=white" className={styles.shield} /></a>&nbsp;&nbsp; 
             <a href="https://docs.9bon.org/" target="_blank"><img src="https://img.shields.io/badge/dev_blog-000000?style=for-the-badge&logo=hugo&logoColor=white" className={styles.shield} /></a>&nbsp;&nbsp;
-            <a href="https://www.youtube.com/channel/UCAEuOSt3IFWM1ga63-KZrLA" target="_blank"><img src="https://img.shields.io/badge/youtube-000000?style=for-the-badge&logo=youtube&logoColor=white" className={styles.shield} /></a>
             <hr />
             <div className={styles.texts}>
-                일편단심 병렬처리, 네트워크-서버 환경개발 공부를 하고 있습니다. <br /><br />
+                온톨로지, 병렬처리, 네트워크-서버 환경개발 공부를 하고 있습니다. <br /><br />
             </div>
             <div style={endHeight} />
         </div>
